52cb6591420405262b4be7cdb27b35ff23fb2a4c
zhouguangsheng <zhouguangsheng@tuniu.com>
2018-07-24 14:40:17 +0800

    spring boot simple

---
+++ spring-boot-simple/src/main/java/org/zhgs/demo/springboot/SpringBootSimpleApplication.java
@@ -0,0 +1,12 @@
+package org.zhgs.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleApplication.class, args);
+	}
+}
---
+++ spring-boot-simple/src/main/java/org/zhgs/demo/springboot/controller/HelloWordController.java
@@ -0,0 +1,15 @@
+package org.zhgs.demo.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "hello spring boot!";
+    }
+}
+
