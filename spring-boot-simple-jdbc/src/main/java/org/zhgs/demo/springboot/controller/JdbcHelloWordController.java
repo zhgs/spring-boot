9fb8f5088ce8d8a67caff0dd030f5ce9958b676b
zhouguangsheng <zhouguangsheng@tuniu.com>
2018-07-27 11:19:38 +0800

    spring-boot-jdbc simple

---
+++ spring-boot-simple-jdbc/src/main/java/org/zhgs/demo/springboot/controller/JdbcHelloWordController.java
@@ -0,0 +1,27 @@
+package org.zhgs.demo.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class JdbcHelloWordController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("list")
+    public List<Map<String, Object>> index(){
+
+
+        List<Map<String, Object>> resultList =  jdbcTemplate.queryForList("select * from test_users");
+
+
+        return resultList;
+
+    }
+}
